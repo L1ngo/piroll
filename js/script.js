45370193b8976637aba6b35fb2553c31d77a15ed
Alexander <dev.redka@izumi-it-company.com>
2020-01-13 17:30:22 +0200

    fixes

--- js/script.js
+++ js/script.js
@@ -1,15 +1,18 @@
+let doc = document;
+
 let ready = (callback) => {
-    if (document.readyState != 'loading') callback();
-    else document.addEventListener('DOMContentLoaded', callback);
+    if (doc.readyState != 'loading') callback();
+    else doc.addEventListener('DOMContentLoaded', callback);
 };
 
 ready(() => {
     transparentHeader();
+    openNav();
 });
 
 function transparentHeader() {
-    let header = document.querySelector('#header');
-    let about = document.querySelector('#about');
+    let header = doc.querySelector('#header');
+    let about = doc.querySelector('#about');
     if (about === null) return;
     window.addEventListener('scroll', function () {
         if (about.getBoundingClientRect().top > header.clientHeight) {
@@ -18,4 +21,17 @@ function transparentHeader() {
             header.classList.add('header_scroll');
         }
     });
+}
+
+function openNav() {
+    let burger          = doc.querySelector('.header-burger');
+    let nav             = doc.querySelector('.main-nav');
+    let checkScrollLock = false;
+
+    if (burger) {
+        burger.addEventListener('click', function () {
+            burger.classList.toggle('header-burger_open');
+            nav.classList.toggle('main-nav_open');
+        });
+    }
 }
